0f9da4be2d099998ab41ae0f92c0ac54195688d5
Nimira43 <n.rabone72@aol.com>
2024-10-17 21:16:36 +0100

    Looping through decimal list and appending the binary values and hex values to their corresponding empty lists.

--- main.py
+++ main.py
@@ -1,6 +1,14 @@
 print('[*]Binary / Hexadecimal Converter')
+
 max_value = int(input('\n[+]Calculate binary and hexadecimal values up to what number?: '))
+
 decimal = list(range(1, max_value + 1))
 binary = []
 hexadecimal = []
+
+for num in decimal:
+  binary.append(bin(num))
+  hexadecimal.append(hex(num))
+
+print('Generating list...')
 
